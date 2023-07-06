18c44444dd15ffc76e41a1e921bd05cba81b91fe
AYGA2K <garouatayoub@gmail.com>
2023-07-06 19:05:55 +0100

    go on with stack

--- stack/stack.go
+++ stack/stack.go
@@ -2,11 +2,29 @@ package stack
 
 type Node struct {
 	value any
-	next  *Node
+	prev  *Node
 }
 
 type Stack struct {
 	Head   *Node
-	Tail   *Node
 	Length int
+}
+
+func (s *Stack) Posh(val any) {
+	node := Node{value: val}
+	s.Length++
+	if s.Head == nil {
+		s.Head = &node
+		return
+	}
+	head := s.Head
+	s.Head = &node
+	s.Head.prev = head
+}
+
+func (s *Stack) Peek() any {
+	if s.Head != nil {
+		return s.Head.value
+	}
+	return nil
 }
